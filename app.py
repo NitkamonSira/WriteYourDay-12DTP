250e8288ca97b1059b40c06784449732a46192c9
NitkamonSira <nitkamonsira07@gmail.com>
2025-05-01 12:46:15 +1200

    create input box to get input for sign up page

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import sqlite3
 
 app = Flask(__name__)
@@ -6,6 +6,14 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("home.html", title = "home")
+
+
+@app.route("/sign_up", methods=["GET", "POST"])
+def sign_up():
+    if request.method == "POST":
+       email = request.form.get("email")
+       return "email: " + email
+    return render_template("sign_up.html")
 
 if __name__ == "__main__":
     app.run(debug = True)
