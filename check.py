a369749f5b59e07a861387cef3b09c9f540ad705
NitkamonSira <nitkamonsira07@gmail.com>
2025-06-06 10:14:02 +1200

    finish check password function and add docstring

--- check.py
+++ check.py
@@ -1,4 +1,13 @@
 def check_email(enter):
+    """Basic check for whether the input is email or not.
+    Check that the input contains '@' or not and check that there are some things before and after the sign.
+
+    Args:
+        enter (str): The input that the user enters as an email.
+
+    Returns:
+        bool: True for the input that pass the basic check and False for not
+    """
     try:
         username, domain = enter.split("@")
         if username != "" and domain != "":
@@ -9,7 +18,15 @@ def check_email(enter):
         return False
     
 def check_password(password):
-    # number, letter(capital+lower) & special letter
+    """Check the input taken met the minimum requirements for password.
+
+    Args:
+        password (str): The input that the user enters as a password.
+
+    Returns:
+        bool: True for the input that meet the requirement, False for not met
+    """
+    condition = 0
     number = False
     capital = False
     lower_letter = False
@@ -24,5 +41,18 @@ def check_password(password):
         elif letter.isascii():
             special = True
             
+    if number:
+        condition += 1
+    if capital:
+        condition += 1
+    if lower_letter:
+        condition += 1
+    if special:
+        condition += 1
+    
+    if len(password) >= 8 and condition >= 3:
+        return True
+    else:
+        return False
     
             
