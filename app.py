9ca4ede11697ed71f96150e696298747fb2a949d
NitkamonSira <nitkamonsira07@gmail.com>
2025-07-22 09:51:56 +1200

    sign in page back end

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, flash, url_for
 from database_connection import *
 from check import *
+from verification import *
 app = Flask(__name__)
 
 @app.route("/")
@@ -17,42 +18,65 @@ def sign_up():
     print(email_entry) #debug
     print(username_entry) #debug
     print(password_entry) #debug
-    email = True
-    username = True
-    password = True
+    email = False
+    username = False
+    password = False
+    register = False
     if request.method == "POST":
         if not check_email(email_entry):
-            email = False
+            email = "this is not an email"
             print("not email") #debug
         elif check_user_data("email", "email", email_entry):
-            email = False
+            email = "email exist"
             print("email exist") #debug
         else:
-            email = True
+            email = False
             print("everything is alright") #debug
             
         if check_user_data("username", "username", username_entry):
-            username = False
-            print("username exist")
+            username = "username exist"
+            print("username exist") #debug
+        elif "@" in username_entry:
+            username = "username cannot contain @"
             
         if not check_password(password_entry):
-            password = False
-            print("password dont meet requirement")
+            password = "password dont meet requirement"
+            print("password dont meet requirement") #debug
         elif password_entry != password_confirm:
-            password = False
-            print("password dont match")
+            password = "password dont match"
+            print("password dont match") #debug
+        
+        print(f"{email} + {username} + {password}")
+        if  (email is not False) or (username is not False) or (password is not False):
+            print("requirement dont pass") #debug
+        else:
+            try:
+                query = """INSERT INTO User (email, username, password)
+    VALUES (?, ?, ?);"""
+                value = (email_entry, username_entry, hash(password_entry))
+                insert_data(query, value)
+                register = True
+                print("insert successful")
+            except sqlite3.IntegrityError:
+                print("fail to insert data") #debug
     
-    
-    if request.method == "POST":
-        try:
-            query = """INSERT INTO Test (email, username, password)
-VALUES (?, ?, ?);"""
-            value = (email_entry, username_entry, hash(password_entry))
-            insert_data(query, value)
-        except sqlite3.IntegrityError:
-            print("fail to insert data")
+    if register:
+        send_email(sender_email, sender_email_password, email_entry, "test")
     
     return render_template("sign_up.html", title = "sign up", email = email, username = username, password = password)
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    username_entry = request.form.get("email")
+    password_entry = request.form.get("password")
+    if request.method == "POST":
+        try:
+            query = ""
+            check_user_data()
+        except sqlite3.IntegrityError:
+            pass
+    
+    return render_template("login.html", title = "login")
 
 if __name__ == "__main__":
     app.run(debug = True)
